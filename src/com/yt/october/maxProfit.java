6fe0ef0a7baf9a1b1614e360b5839474fd02fc17
BigBeardYT <627944524@qq.com>
2023-10-01 10:50:00 +0800

    "leetcode 1333 commit"

---
+++ src/com/yt/october/maxProfit.java
@@ -0,0 +1,18 @@
+package com.yt.october;
+
+public class maxProfit {
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+    }
+    public static int maxProfit(int[] prices) {
+        int n = prices.length;
+        int [] dp = new int[n];
+        dp[0] = 0;
+        int min_price = prices[0];
+        for(int i = 1;i < n;i++){
+            min_price = Math.min(min_price, prices[i]);
+            dp[i] = Math.max(dp[i-1], prices[i] - min_price);
+        }
+        return dp[n-1];
+    }
+}
