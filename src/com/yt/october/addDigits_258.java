6fdf3cf0b5f6615cd812af9eb0e61f59504ecc64
BigBeardYT <627944524@qq.com>
2023-10-17 18:32:43 +0800

    "leetcode commit"

---
+++ src/com/yt/october/addDigits_258.java
@@ -0,0 +1,26 @@
+package com.yt.october;
+
+public class addDigits_258 {
+    public static void main(String[] args) {
+        int num = 38;
+        int res = addDigits(num);
+        System.out.println(res);
+    }
+    public static int addDigits(int num) {
+        int res = 0;
+        do{
+            res = fun(num);
+            num = res;
+        }while(res > 9);
+
+        return res;
+    }
+    public static int fun(int num) {
+        int res = 0;
+        while(num != 0) {
+            res += num % 10;
+            num /= 10;
+        }
+        return res;
+    }
+}
